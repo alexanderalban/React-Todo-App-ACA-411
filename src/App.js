dbe409cb132d3024f4c91723c8f9729fce015169
alexanderalban <albanalexander@gmail.com>
2021-01-18 19:36:33 -0600

    list adds items properly:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './firstComponent';
 
-function App() {
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     isClicked: false
+  //   }
+  // }
+
+  // toggle = () => {
+  //   this.setState({isClicked: !this.state.isClicked})
+  // }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+      items: []
+    }
+  }
+
+  inputUpdate = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  formSubmit = event => {
+    event.preventDefault()
+    console.log("THIS IS STATE", this.state.items)
+    this.setState({
+      items : [...this.state.items, this.state.input],
+      input: ''
+    })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.input} onChange={this.inputUpdate}></input>
+          <button>Submit</button>
+        </form>
+        {/* {<button onClick={this.toggle}>{this.state.isClicked ? 'On' : 'Off'}</button>} */}
+      <FirstComponent items={this.state.items}></FirstComponent>
       </header>
     </div>
   );
+}
 }
 
 export default App;
